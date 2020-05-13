fb8fbda6b836ee79bdc51e41c1ffcf0d2301451a
Maxim Nikulin <Madnikulin50@gmail.com>
2020-05-13 18:36:57 +0300

    fix

--- IAM/send-1c-soap.ps1
+++ IAM/send-1c-soap.ps1
@@ -1,5 +1,6 @@
 param (
     [string]$URL = "https://1cweb.tass.ru/zup_centr/ws/Upload.1cws",
+    [string]$fn = "c:\work\res.xml",
     [string]$request = @"
     <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
         <Body>
@@ -7,11 +8,14 @@ param (
         </Body>
     </Envelope>
 "@,
-    [string]$makves_url = "", ##"http://10.0.0.10:8000",
-    [string]$makves_user = "admin",
-    [string]$makves_pwd = "admin"
-)
+[string]$makves_url = "http://127.0.0.1:8000",
+[string]$makves_user = "admin",
+[string]$makves_pwd = "admin"
 
+)
+if (Test-Path $fn) {
+    Remove-Item $fn
+}
 
 ## Init web server 
 $uri = $makves_url + "/identity/upload"
@@ -22,49 +26,25 @@ $base64 = [System.Convert]::ToBase64String($bytes)
 
 $basicAuthValue = "Basic $base64"
 
-$headers = @{ Authorization = $basicAuthValue }
+$headers = @{ Authorization = $basicAuthValue}
 
 if ($makves_url -eq "") {
     $uri = ""
     Add-Type -AssemblyName 'System.Net.Http'
 }
+
+
  
-#$cert = Get-ChildItem -Path Cert:\LocalMachine\My | where-Object {$_.Subject -like 'Subject of certificate'} 
- 
-#if($cert -ne $null)
-#{
-    Try
-    {
- 
-        # Sending SOAP Request To Server 
-        $soapWebRequest = [System.Net.WebRequest]::Create($URL) 
-        $soapWebRequest.ClientCertificates.Add($cert)
-        $soapWebRequest.Headers.Add("SOAPAction","Provide method name of the API")
-        $soapWebRequest.ContentType = "text/xml;charset=utf-8"
-        $soapWebRequest.Accept      = "text/xml"
-        $soapWebRequest.Method      = "POST"
-        $soapWebRequest.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
-        $soapWebRequest.UseDefaultCredentials = $true
-    
-        #Initiating Send
-        $requestStream = $soapWebRequest.GetRequestStream() 
-        $SOAPRequest.Save($requestStream) 
-        $requestStream.Close() 
-       
-        #Send Complete, Waiting For Response.
-        $resp = $soapWebRequest.GetResponse() 
-        $responseStream = $resp.GetResponseStream() 
-        $soapReader = [System.IO.StreamReader]($responseStream) 
-        $ReturnXml = [Xml] $soapReader.ReadToEnd() 
-        $responseStream.Close() 
- 
-    }
-    Catch
-    {
-        Throw $ReturnXml.Envelope.InnerText
-    }
- 
-    $Body = $ReturnXml.Envelope.InnerText
+
+$resp = Invoke-WebRequest -Uri $URL -Headers (@{SOAPAction='Read'
+Authorization='Basic ЧЧЧЧЧЧЧЧ='}) -Method Post -Body $request -ContentType application/xml 
+
+$body = [System.Text.Encoding]::UTF8.GetBytes($resp.Content);
+
+$body | Out-File -FilePath $fn
+
+$Body = $ReturnXml.Envelope.InnerText
+    $fname = "1c.xml"
     $boundary = [guid]::NewGuid().ToString()
     $template = "
     --$boundary
@@ -79,19 +59,13 @@ if ($makves_url -eq "") {
 
     --$boundary--"
 
-    Try {
-        $upload_file = Invoke-RestMethod -Uri $uri -Method POST -Headers $headers -Body $template -ContentType "multipart/form-data;boundary=$boundary" -Verbose
-        Write-Host  "Send data to server:" + $uri + " res:" $upload_file
-    }
-    Catch {
-        Write-Host "Error send data to server:" + $PSItem.Exception.Message
-    }
+Try {
+    $upload_file = Invoke-RestMethod -Uri $uri -Method POST -Headers $headers -Body $template -ContentType "multipart/form-data;boundary=$boundary" -Verbose
+    Write-Host  "Send data to server:" + $uri + " res:" $upload_file
+}
+Catch {
+    Write-Host "Error send data to server:" + $PSItem.Exception.Message
+}
 
-    
- 
-#else
-#{
-#    $Return = "Certificate not found"
-#}
- 
-#$SOAPRequest = $SOAPRequest.OuterXML
+Write-Host 'done'
+
